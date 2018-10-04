1d1cf9d6ff198ab1d8e36d24dcfc1d7179853a4a
baxinski <andrebaxinski@gmail.com>
2018-10-04 14:30:05 -0300

    Add files via upload

---
+++ formulario-de-contato/conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$dbname = "POAPODAS";
+	
+	//Criar a conexão
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+?>
